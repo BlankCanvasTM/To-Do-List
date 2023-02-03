3dc724c9fde22b2b0bd9964e4c731c4a2b4081b0
BlankCanvasTM <BlankCanvasTM@users.noreply.github.com>
2023-02-03 18:33:29 +0000

    Add items complete with icons title and priority

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 
 const addButton = document.querySelector('.addButton')
 let count = 0;
-let IconCount = 0;
+let checkCount = "0";
+let crossCount = "0";
+let iconCount = "0";
 
 addButton.addEventListener('click', function() {
     
@@ -19,8 +21,7 @@ addButton.addEventListener('click', function() {
 
     //Add the priority level
     const priorityText = document.createElement('h2');
-    priorityText.setAttribute('class','priority');
-    priorityText.setAttribute('class',priorityValue);
+    priorityText.setAttribute('class','priority ' + priorityValue);
     priorityText.textContent = priorityValue;
     document.getElementById(count).append(priorityText);
 
@@ -30,41 +31,37 @@ addButton.addEventListener('click', function() {
     titleText.textContent = titleTextValue;
     document.getElementById(count).append(titleText);
 
-    //Add icons to the item
+    //Create a span element. add icon class for styling then add it to the current to do item.
     const checkSpan = document.createElement('span');
     checkSpan.setAttribute('class', 'icon');
-    checkSpan.setAttribute('id', IconCount);
-    
-    
-    
+    checkSpan.setAttribute('id', "iconCount" + iconCount);
     document.getElementById(count).append(checkSpan);
     
+    
+    
+    //Add the icon image to the span class, by adding the necessary classes
     const checkIcon = document.createElement('i');
-    checkIcon.setAttribute('class', 'fa-solid');
-    checkIcon.setAttribute('class', 'fa-check');
+    checkIcon.setAttribute('class', 'fa-solid' + ' fa-check');
     
     
 
-    document.getElementById(IconCount).append(checkIcon);
-    document.getElementsByClassName('checkmark').append(checkIcon);
-    
-    /* document.getElementById(count).append(checkSpan);
-    document.getElementById(count += 'check').append(checkIcon);
+    document.getElementById("iconCount" + iconCount).append(checkIcon);
+    iconCount++;
+
 
     const crossSpan = document.createElement('span');
     crossSpan.setAttribute('class', 'icon');
-    crossSpan.setAttribute('id', count += 'check');
-    const crossIcon = document.createElement('i');
-    crossIcon.setAttribute('class', 'fa-sharp');
-    crossIcon.setAttribute('class', 'fa-solid');
-    crossIcon.setAttribute('class', 'fa-xmark');
-
+    crossSpan.setAttribute('id', "crossIcon" + iconCount);
     document.getElementById(count).append(crossSpan);
-    document.getElementById(count += 'check').append(crossIcon); */
 
+    const crossIcon = document.createElement('i');
+    crossIcon.setAttribute('class', 'fa-solid' + ' fa-sharp' + ' fa-xmark');
+    document.getElementById("crossIcon" + iconCount).append(crossIcon);
 
     
     count++;
+    checkCount++;
+    crossCount++;
 
 
 })
