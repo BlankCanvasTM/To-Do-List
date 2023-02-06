eb27010d48b3b53b32d8f3271c03856087012837
BlankCanvasTM <BlankCanvasTM@users.noreply.github.com>
2023-02-06 20:29:46 +0000

    add task button pt1

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-const addButton = document.querySelector('.addButton')
+const addButton = document.querySelector('.addButton');
+
+addButton.addEventListener('click', function() {
+    console.log("icp")
+    
+    let taskInputText = document.getElementById("taskInput");
+    
+    if(taskInputText.value.length == 0)
+        alert("Enter a Task!")
+    else{
+        document.querySelector('.container').innerHTML += `
+        <div class="item">
+        <h2>${taskInputText.value}</h2>
+        <span class="icon"><i class="fa-solid fa-trash-can"></i></span>
+        </div
+        
+        
+        `
+
+        /* const item = `
+        <div class="item">
+        <h2>${taskInputText.value}</h2>
+        <span class="icon"><i class="fa-solid fa-trash-can"></i></span>
+        </div
+        
+        
+        `
+        document.body.innerHTML = item;//document.getElementById('containerBox').append(item); */
+    }
+
+
+
+
+});
